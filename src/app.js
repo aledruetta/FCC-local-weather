7eba0ecae4d5beba8f7494d7253eea86f875e825
Alejandro Druetta <aledruetta@gmail.com>
2017-06-09 15:28:53 -0300

    wallpaper image

---
+++ src/app.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  var openMapApiKey = 'b09e2b4e361fecae02a85343a8398e5c';
+  var openMapWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={api_key}';
+  // {layer} layer name
+  // {z} number of zoom level
+  // {x} number of x tile coordinate
+  // {y} number of y tile coordinate
+  // {api_key} Your API key
+  var openMapLayerUrl = 'http://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid={api_key}';
+
+
+});
