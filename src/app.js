0ee1a48b8f61e5ae17553ae3b655aa9bc8716b3a
Alejandro Druetta <aledruetta@gmail.com>
2017-06-10 17:15:17 -0300

    slide pictures

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 $(function() {
+  "use strict";
 
   var openMapApiKey = 'b09e2b4e361fecae02a85343a8398e5c';
   var openMapWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={api_key}';
@@ -11,9 +12,28 @@ $(function() {
 
   var AppViewModel = function() {
 
-    this.backgroundUrl = ko.observable(getRandomBackground());
+    var self = this;
+    this.backgroundUrl = ko.observable('');
 
+    window.setInterval(function() {
+      var back = getRandomBackground();
+      self.backgroundUrl(back);
+    }, 7000);
   };
+
+  ko.bindingHandlers.fadeBackground = {
+    update: function(element, valueAccessor) {
+      var url = ko.unwrap(valueAccessor());
+      var $div = $(element);
+
+      $div.fadeToggle(800, function() {
+        $div.css('background-image', 'url(' + url + ')');
+        $div.fadeToggle(800);
+      });
+    }
+  };
+
+  ko.applyBindings(new AppViewModel());
 
   function getRandomBackground(seasson) {
     var list = ['winter', 'spring', 'summer', 'fall'];
@@ -25,7 +45,4 @@ $(function() {
 
     return 'img/' + list[seasson] + '-0' + num + '.jpg';
   }
-
-  ko.applyBindings(new AppViewModel());
-
 });
