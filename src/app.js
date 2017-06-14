e114cf4c29ee68906259532015a6a35906387c46
Alejandro Druetta <aledruetta@gmail.com>
2017-06-14 15:27:42 -0300

    getWeather function

--- src/app.js
+++ src/app.js
@@ -4,54 +4,12 @@ $(function() {
   var apixuKey = '91f999f0561d4d7a9b3155126171406';
   var apixuUrl = 'https://api.apixu.com/v1/current.json?key={key}&q={lat},{lon}';
 
-  var getWeather = new Promise(function(resolve, reject) {
-    if ('geolocation' in navigator) {
-      var options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-      };
-
-      navigator.geolocation.getCurrentPosition(success, error, options);
-
-    } else {
-      reject('Geolocation is not enabled');
-    }
-
-    function success(position) {
-      var weather = {};
-      var coord = {lat: position.coords.latitude, lon: position.coords.longitude};
-      var url = apixuUrl
-        .replace(/{key}/, apixuKey)
-        .replace(/{lat}/, coord.lat)
-        .replace(/{lon}/, coord.lon);
-
-      $.getJSON(url, function(data) {
-        console.log(data);
-        weather.city = data.location.name;
-        weather.country = data.location.country;
-        weather.region = data.location.region;
-        weather.temp_c = data.current.temp_c;
-        weather.temp_f = data.current.temp_f;
-        weather.humidity = data.current.humidity;
-        weather.precip = data.current.precip_mm;
-        weather.pressure = data.current.pressure_mb;
-        weather.visibility = data.current.vis_mk;
-        weather.wind_kph = data.current.wind_kph;
-        weather.wind_dir = data.current.wind_dir;
-        weather.clouds = data.current.cloud;
-        resolve(weather);
-      });
-    }
-
-    function error(err) {
-      console.warn(err.code, err.message);
-    }
-  });
-
+  // ViewModel
   var AppViewModel = function() {
     var self = this;
+    var promise = getWeather();
 
+    // Observables
     this.backgroundUrl = ko.observable(getRandomUrl());
     this.weatherIcon = ko.observable('wi wi-day-cloudy-gusts');
     this.cityName = ko.observable('');
@@ -59,10 +17,11 @@ $(function() {
     this.temp = ko.observable('');
     this.unit = ko.observable('celsius');
 
-    getWeather.then(function(weather) {
+    promise.then(function(weather) {
       console.log(JSON.stringify(weather));
       self.cityName(weather.city);
       self.detailsPosition(weather.region + ', ' + weather.country);
+
       if (self.unit('celsius')) {
         self.temp(weather.temp_c);
       } else {
@@ -97,5 +56,53 @@ $(function() {
     }
 
     return 'img/' + list[seasson] + '-0' + num + '.jpg';
+  }
+
+  function getWeather() {
+    return new Promise(function(resolve, reject) {
+      if ('geolocation' in navigator) {
+        var options = {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0
+        };
+
+        navigator.geolocation.getCurrentPosition(success, error, options);
+      } else {
+        reject(Error('Geolocation is not enabled'));
+      }
+
+      function success(position) {
+        var json = {};
+        var coord = {lat: position.coords.latitude, lon: position.coords.longitude};
+        var url = apixuUrl
+        .replace(/{key}/, apixuKey)
+        .replace(/{lat}/, coord.lat)
+        .replace(/{lon}/, coord.lon);
+
+        $.getJSON(url, function(data) {
+          console.log(data);
+
+          json.city = data.location.name;
+          json.country = data.location.country;
+          json.region = data.location.region;
+          json.temp_c = data.current.temp_c;
+          json.temp_f = data.current.temp_f;
+          json.humidity = data.current.humidity;
+          json.precip = data.current.precip_mm;
+          json.pressure = data.current.pressure_mb;
+          json.visibility = data.current.vis_mk;
+          json.wind_kph = data.current.wind_kph;
+          json.wind_dir = data.current.wind_dir;
+          json.clouds = data.current.cloud;
+
+          resolve(json);
+        });
+      }
+
+      function error(err) {
+        console.warn('Warn: error', err.code, err.message);
+      }
+    });
   }
 });
