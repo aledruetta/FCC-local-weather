2f9051c3c363840ffe269bf1a633d248e06937ab
Alejandro Druetta <aledruetta@gmail.com>
2017-06-14 12:04:12 -0300

    get json api

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ $(function() {
   "use strict";
 
   var openMapApiKey = 'b09e2b4e361fecae02a85343a8398e5c';
-  var openMapWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={api_key}';
+  var openMapWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}';
   // {layer} layer name
   // {z} number of zoom level
   // {x} number of x tile coordinate
   // {y} number of y tile coordinate
   // {api_key} Your API key
   var openMapLayerUrl = 'http://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid={api_key}';
+
+  var weather = getWeather();
+  console.log(weather);
 
   var AppViewModel = function() {
     var self = this;
@@ -47,5 +50,36 @@ $(function() {
     }
 
     return 'img/' + list[seasson] + '-0' + num + '.jpg';
+  }
+
+  function getWeather() {
+    return new Promise(function(resolve, reject) {
+      if ('geolocation' in navigator) {
+        var weather = {};
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var coord = {lat: position.coords.latitude, lon: position.coords.longitude};
+          var url = openMapWeatherUrl
+            .replace(/{api_key}/, openMapApiKey)
+            .replace(/{lat}/, coord.lat)
+            .replace(/{lon}/, coord.lon);
+
+          weather.json = $.getJSON(url, function(data) {
+            weather.city = data.name;
+            weather.country = data.sys.country;
+            weather.temp = data.main.temp;
+            weather.temp_min = data.main.temp_min;
+            weather.temp_max = data.main.temp_max;
+            weather.humidity = data.main.humidity;
+            weather.pressure = data.main.pressure;
+            weather.wind = data.wind;
+            weather.clouds = data.clouds.all;
+            resolve(weather);
+          });
+        });
+      } else {
+        reject('Geolocation is not enabled');
+      }
+    });
   }
 });
