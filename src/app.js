ccaa5f2405f6eb2dfca09ee59b070bfa4d3849b2
Alejandro Druetta <aledruetta@gmail.com>
2017-06-14 13:37:39 -0300

    migrating to apixu weather api

--- src/app.js
+++ src/app.js
@@ -1,40 +1,51 @@
 $(function() {
   "use strict";
 
-  var openMapApiKey = 'b09e2b4e361fecae02a85343a8398e5c';
-  var openMapWeatherUrl = 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}';
-  // {layer} layer name
-  // {z} number of zoom level
-  // {x} number of x tile coordinate
-  // {y} number of y tile coordinate
-  // {api_key} Your API key
-  var openMapLayerUrl = 'https://cors-anywhere.herokuapp.com/http://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid={api_key}';
+  var apixuKey = '91f999f0561d4d7a9b3155126171406';
+  var apixuUrl = 'https://api.apixu.com/v1/current.json?key={key}&q={lat},{lon}';
 
   var getWeather = new Promise(function(resolve, reject) {
     if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var weather = {};
-        var coord = {lat: position.coords.latitude, lon: position.coords.longitude};
-        var url = openMapWeatherUrl
-          .replace(/{api_key}/, openMapApiKey)
-          .replace(/{lat}/, coord.lat)
-          .replace(/{lon}/, coord.lon);
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
 
-        $.getJSON(url, function(data) {
-          weather.city = data.name;
-          weather.country = data.sys.country;
-          weather.temp = data.main.temp;
-          weather.temp_min = data.main.temp_min;
-          weather.temp_max = data.main.temp_max;
-          weather.humidity = data.main.humidity;
-          weather.pressure = data.main.pressure;
-          weather.wind = data.wind;
-          weather.clouds = data.clouds.all;
-          resolve(weather);
-        });
-      });
+      navigator.geolocation.getCurrentPosition(success, error, options);
+
     } else {
       reject('Geolocation is not enabled');
+    }
+
+    function success(position) {
+      var weather = {};
+      var coord = {lat: position.coords.latitude, lon: position.coords.longitude};
+      var url = apixuUrl
+        .replace(/{key}/, apixuKey)
+        .replace(/{lat}/, coord.lat)
+        .replace(/{lon}/, coord.lon);
+
+      $.getJSON(url, function(data) {
+        console.log(data);
+        weather.city = data.location.name;
+        weather.country = data.location.country;
+        weather.region = data.location.region;
+        weather.temp_c = data.current.temp_c;
+        weather.temp_f = data.current.temp_f;
+        weather.humidity = data.current.humidity;
+        weather.precip = data.current.precip_mm;
+        weather.pressure = data.current.pressure_mb;
+        weather.visibility = data.current.vis_mk;
+        weather.wind_kph = data.current.wind_kph;
+        weather.wind_dir = data.current.wind_dir;
+        weather.clouds = data.current.cloud;
+        resolve(weather);
+      });
+    }
+
+    function error(err) {
+      console.warn(err.code, err.message);
     }
   });
 
@@ -44,14 +55,19 @@ $(function() {
     this.backgroundUrl = ko.observable(getRandomUrl());
     this.weatherIcon = ko.observable('wi wi-day-cloudy-gusts');
     this.cityName = ko.observable('');
-    this.detailsPosition = ko.observable('SP, Brazil');
+    this.detailsPosition = ko.observable('');
     this.temp = ko.observable('');
     this.unit = ko.observable('fahrenheit');
 
     getWeather.then(function(weather) {
       console.log(JSON.stringify(weather));
       self.cityName(weather.city);
-      self.temp(weather.temp);
+      self.detailsPosition(weather.region + ', ' + weather.country);
+      if (self.unit === 'celsius') {
+        self.temp(weather.temp_c);
+      } else {
+        self.temp(weather.temp_f);
+      }
     });
   };
 
